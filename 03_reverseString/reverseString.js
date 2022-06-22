479c4990a7cac086a5ed65593873aeb7999597bd
nathanjpak <nathan.j.pak@gmail.com>
2022-06-22 16:43:21 -0700

    finish reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const array = string.split("");
+    let reversed = "";
+    for (i = array.length - 1; i >= 0; i--) {
+        reversed += array[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
