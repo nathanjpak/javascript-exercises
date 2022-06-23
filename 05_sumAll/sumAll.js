95b48b8eca2a2129803fa0849ff821f83df22aa6
nathanjpak <nathan.j.pak@gmail.com>
2022-06-22 17:15:30 -0700

    finish sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof(num1) === "number" && typeof(num2) === "number") {
+        let start = num1;
+        let end = num2;
+        if (num2 < num1) {
+            start = num2;
+            end = num1;
+        }
+        if (start >= 0 && end >= 0) {
+            let num = 0;
+            for (i=start; i<=end; i++) {
+                num = num+i;
+            }
+            return num;
+        } else {
+            let message = "ERROR";
+            return message; 
+        }
+    } else {
+        let message = "ERROR";
+        return message;
+    }
 };
 
 // Do not edit below this line
