683f8a2f38656909af2f72912ab68a5a57a75fba
nathanjpak <nathan.j.pak@gmail.com>
2022-06-22 17:05:19 -0700

    finish remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...unwanteds) {
+    const newArray = [];
+    for (element of array) {
+        let matches = 0;
+        for (unwanted of unwanteds) {
+            if (element === unwanted) {
+                matches++;
+            }
+        }
+        if (matches === 0) {
+            newArray.push(element);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
