4c8cc6f9e7f1617e16f506294a5b98ca8c33349a
nathanjpak <nathan.j.pak@gmail.com>
2022-06-22 17:27:03 -0700

    finish temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celsius = (temp - 32) * (5/9);
+  celsius = Math.round(celsius*10)/10;
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let fahrenheit = (temp * 9/5) + 32;
+  fahrenheit = Math.round(fahrenheit*10)/10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
