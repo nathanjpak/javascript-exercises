0022f8c243c4d1780e7cd1b120e08ef939a06bcb
nathanjpak <nathan.j.pak@gmail.com>
2022-06-30 16:48:06 -0700

    finish calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    let init = 0;
+    return array.reduce((previousValue, currentValue) => previousValue + currentValue, init);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+    let init = 1;
+    return array.reduce((previous, current) => previous * current, init);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+    let value = 1;
+    for (i = 1; i <= b; i++) {
+        value = value * a;
+    }
+    return value;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+        return 1;
+    }
+    let fact = 1;
+    for (i = num; i > 1; i--) {
+        fact = fact * i;
+    }
+    return fact;
 };
 
 // Do not edit below this line
