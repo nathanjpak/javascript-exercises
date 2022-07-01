b23e67fdcaf5ead915098a2ea6d262c58860c6a6
nathanjpak <nathan.j.pak@gmail.com>
2022-06-30 17:31:05 -0700

    finish fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    if (position <= 0) {
+        return "OOPS";
+    } else if (position <= 2) {
+        return 1;
+    } else {
+        const fibs = [1, 1];
+        for (i=2; i < position; i++) {
+            fibs.push(fibs[i-1]+fibs[i-2]);
+        }
+        return fibs[position-1];
+    }
 };
 
 // Do not edit below this line
