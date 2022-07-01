f9f0a97d5f2480090eb7051347eb3770c970a6fe
nathanjpak <nathan.j.pak@gmail.com>
2022-06-30 17:19:41 -0700

    finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const punctuation = [".", ",", "!", "?", " "];
+    string = string.replace(/[^a-zA-Z]/g, "");
+    string = string.toLowerCase();
+    let midpoint = Math.floor(string.length / 2);
+    const firstHalf = string.slice(0, midpoint);
+    let secondHalf = "";
+    if ((string.length % 2) === 0) {
+        secondHalf = string.slice(midpoint);
+    } else {
+        secondHalf = string.slice(midpoint + 1);
+    }
+    for (i=0; i < firstHalf.length; i++) {
+        if (firstHalf[i] !== secondHalf[firstHalf.length - i -1]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
