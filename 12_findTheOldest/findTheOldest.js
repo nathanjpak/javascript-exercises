63b899a9765d7dda828ab2a152f52352d05a808b
nathanjpak <nathan.j.pak@gmail.com>
2022-06-30 17:59:02 -0700

    finish find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const sorted = people.sort(function(a, b) {
+        if (!(a.yearOfDeath)) {
+            a.yearOfDeath = (new Date()).getFullYear();
+        }
+        if (!(b.yearOfDeath)) {
+            b.yearOfDeath = (new Date()).getFullYear();
+        }
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return (lastPerson > nextPerson) ? -1 : 1;
+    });
+    return sorted[0];
 };
 
 // Do not edit below this line
